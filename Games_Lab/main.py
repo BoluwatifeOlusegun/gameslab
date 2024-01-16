23b7156c8cf1b40b30ae09dced40fb45143f4673
Tife <99103434+BoluwatifeOlusegun@users.noreply.github.com>
2024-01-16 04:18:33 -0400

    Add files via upload

---
+++ Games_Lab/main.py
@@ -0,0 +1,20 @@
+#Prompt user for their name 
+user = input("To Proceed, Enter your 'Nickname' : ")
+
+#Validate users input
+if user == user.lower():
+    pass
+elif user == int:
+    print("Numbers not allowed, Use Letters!!!")
+
+elif user == None:
+    print("Error!!!")
+else:
+    print("Kindly Enter your username")
+
+#Welcome Message
+print(f"Hello {user}, Welcome to Games Lab")
+
+'''
+Ask user for their gaming platform.
+'''
